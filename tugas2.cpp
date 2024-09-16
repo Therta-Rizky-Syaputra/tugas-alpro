37881aecd6b27c3c70f4d15291bf0e3a5772071c
Therta-Rizky-Syaputra <2410631170114@student.unsika.ac.id>
2024-09-16 23:36:05 +0700

    Add files via upload

---
+++ tugas2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+    cout << "Masukkan nilai Anda: ";
+    cin >> score;
+
+    if (score >= 90) {
+        cout << "Selamat! Anda mendapatkan nilai A." << endl;
+    } else if (score >= 80) {
+        cout << "Anda mendapatkan nilai B." << endl;
+    } else if (score >= 70) {
+        cout << "Anda mendapatkan nilai C." << endl;
+    } else if (score >= 60) {
+        cout << "Anda mendapatkan nilai D." << endl;
+    } else {
+        cout << "Anda mendapatkan nilai E." << endl;
+    }
+
+    return 0;
+}
